ede9514f49c5c65ae3c3a49448a29f2a6a30e0a3
Tom Alexander <craftkiller@gmail.com>
2012-12-01 01:14:12 -0500

    removed old types

--- src/types.h
+++ src/types.h
@@ -39,34 +39,6 @@ typedef uint_fast64_t fu64;
 //intptr_t
 //intmax_t
 
-#if 0
-#if defined __GNUG__ && !(defined __clang__)
-#define GCC
-#warning Using GCC, swizzles will be slow, you should try clang!
-    #endif
-#if defined __clang__
-#define CLANG
-    #endif
-#else
-#define GCC
-#endif
-
-typedef float v16sf __attribute__ ((vector_size (sizeof(float)*16)));
-typedef float v4sf __attribute__ ((vector_size (sizeof(float)*4)));
-typedef int v4si __attribute__ ((vector_size (sizeof(int)*4)));
-typedef union matrix_data
-{
-    v16sf full_matrix;
-    v4sf matrix_vectors[4];
-    float matrix_floats[16];
-} matrix_data;
-
-typedef union vector_data
-{
-    v4sf full_vector;
-    float vector_floats[4];
-} vector_data;
-
 static const double PI = 3.14159265358979323846;
 #define TO_RADIANS(DEG) ((DEG) * PI / 180.0f)
 #define TO_DEGREES(RAD) ((RAD) * 180.0f / PI)
