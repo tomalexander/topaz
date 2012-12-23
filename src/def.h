bf844f1ce28721c73df0b339d77bfb8f57a4c8e3
Tom Alexander <craftkiller@gmail.com>
2012-12-23 18:06:57 -0500

    one more print flags

--- src/def.h
+++ src/def.h
@@ -51,5 +51,6 @@
 #define CHECK_FLAGS(PRINT_FLAGS) ((ACTIVE_FLAGS & PRINT_FLAGS) == PRINT_FLAGS)
 #define CHECK_FLAGS_ANY(PRINT_FLAGS) (ACTIVE_FLAGS & PRINT_FLAGS != 0)
 #define debug_flags(PRINT_FLAGS, ...) if (CHECK_FLAGS(PRINT_FLAGS)) printf(__VA_ARGS__)
+#define debug_flags_any(PRINT_FLAGS, ...) if (CHECK_FLAGS_ANY(PRINT_FLAGS)) printf(__VA_ARGS__)
 
 #endif
